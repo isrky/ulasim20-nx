bc277451006dd6fe2f74b0dd909a4beead752f58
İsmail Sarıkaya <142857944+isrky@users.noreply.github.com>
2026-07-04 23:44:02 +0300

    feat(ui-map): export MapPopup and useMapInstance

--- libs/ui/map/src/index.ts
+++ libs/ui/map/src/index.ts
@@ -1,2 +1,4 @@
-export { MapView } from './lib/map-view'
+export { MapView, OPENFREEMAP_STYLE } from './lib/map-view'
 export { MapMarker } from './lib/map-marker'
+export { MapPopup } from './lib/map-popup'
+export { MapInstanceProvider, useMapInstance } from './lib/use-map-instance'
